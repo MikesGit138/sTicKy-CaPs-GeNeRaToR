4b1255c89d7cac07e5948db707c3a6a51f67df4c
MikesGit138 <michael138leighton@gmail.com>
2022-06-14 09:43:01 -0500

    functionality added

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
-let text = 'software engineer'
-let result = ''
+function generate() {
+    let text = document.getElementById('text').value
+    let result = document.getElementById('result')
 
-for(let i = 0; i < text.length; i++){
-    if(i % 2 === 0 || i === 0){
-        result = text[i].toLowerCase()
-    }else{
-        result = text[i].toUpperCase()
+    for (let i = 0; i < text.length; i++) {
+        if (i % 2 === 0 || i === 0) {
+            result.textContent += text[i].toLowerCase()
+        } else {
+            result.textContent += text[i].toUpperCase()
+        }
+
     }
-    console.log(result)
+
+}
+
+function newWord(){
+    let result = document.getElementById('result')
+    result.textContent = ''
 }
